a94efb29228e57ceb3e6ae1539e860aeff6f9f07
jaywcjlove <398188662@qq.com>
2024-09-29 16:37:19 +0800

    refactor: modify output log.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -122,7 +122,7 @@ export async function createHTML(files: IFileDirStat[] = [], opts: Options, num 
     await fs.writeFile(SEARCH_DATA_JS, `const REFS_DATA = ${JSON.stringify(resultSearchData)}`);
   }
   await fs.writeFile(outputHTMLPath, html as string);
-  console.log(`♻️ \x1b[32;1m ${path.relative(opts.output, outputHTMLPath)} \x1b[0m`);
+  console.log(`♻️ \x1b[32;1m ${path.relative(path.resolve(opts.output, '..'), outputHTMLPath)} \x1b[0m`);
   createHTML(files, opts, num);
 }
 
